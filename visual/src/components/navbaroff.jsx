18b359be1ad9250f2df3d162723fad3aceee4a9d
oliveira533 <go609127@gmail.com>
2023-04-08 12:39:22 -0300

    Adicionei as listas da barra de navegaçãoo

--- visual/src/components/navbaroff.jsx
+++ visual/src/components/navbaroff.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import '../colors.css'
 import './navbaroff.css';
 import { FaUser  } from "react-icons/fa";
-import { TbHomeDot  } from "react-icons/tb";
+import { FiLogOut  } from "react-icons/fi";
+import { RiHome4Fill  } from "react-icons/ri";
+import { BiSearchAlt2  } from "react-icons/bi";
+import { BsFillGearFill  } from "react-icons/bs";
 
 function NavOff(){
     return(
@@ -11,10 +14,13 @@ function NavOff(){
             <FaUser className='font-color icon-user'/>
             <h2 className='font-color'>User name</h2>
             </div>
-            <ul className='list'>
-                <li><TbHomeDot/> Home</li>
-                <li></li>
-                <li></li>
+            <ul className='list1 font-color'>
+                <li className='item-list'><RiHome4Fill/> Home</li>
+                <li className='item-list'><BiSearchAlt2/> Pesquisar</li>
+            </ul>
+            <ul className='list2 font-color'>
+                <li className='item-list'><BsFillGearFill/> Configuração</li>
+                <li className='item-list'><FiLogOut/> Sair</li>
             </ul>
         </nav>
     )
