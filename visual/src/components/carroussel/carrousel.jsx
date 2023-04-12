eebbb769785d172943e7c068ad490c52435c241f
oliveira533 <go609127@gmail.com>
2023-04-12 18:48:49 -0300

    Criando a pasta do carrosel

--- visual/src/components/carroussel/carrousel.jsx
+++ visual/src/components/carroussel/carrousel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import "../colors.css";
+import "../../colors.css";
 import "./carrousel.css";
 
 function Carrousel(){
--- visual/src/components/mainoff.jsx
+++ visual/src/components/mainoff.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './mainoff.css';
-import Carrousel from './carrousel';
+import Carrousel from './carroussel/carrousel.jsx';
 
 function MainOff(){
     return(
