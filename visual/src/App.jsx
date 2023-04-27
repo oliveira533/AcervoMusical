b5f38608339387254e71dc848959d27b0d85d131
oliveira533 <go609127@gmail.com>
2023-04-27 08:34:30 -0300

    Tentando ajustar a tela

--- visual/src/App.jsx
+++ visual/src/App.jsx
@@ -1,18 +1,16 @@
 import './App.css';
 import './colors.css';
-import NavOff from './components/navbar/navbaroff';
-import MainOff from './components/main/mainoff';
-import NavIn from './components/navbar/navbarin';
-import Search from './components/search/search';
-import Favorite from './components/favorite/favorite';
+import NavOff from './components/navbar/navbaroff.jsx';
+import MainOff from './components/main/mainoff.jsx';
+import NavIn from './components/navbar/navbarin.jsx';
+import Search from './components/search/search.jsx';
+import Favorite from './components/favorite/favorite.jsx';
 
 function App() {
   return (
     <div className="App main-color">
       <NavIn/>
-      <main>
-        <Favorite/>
-      </main>
+      <Favorite/>
     </div>
   );
 }
