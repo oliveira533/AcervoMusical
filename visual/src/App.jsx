67bf57ad258707745d14fc86ba67a616fd0062be
oliveira533 <go609127@gmail.com>
2023-04-27 09:10:52 -0300

    Vou recomeçar as telas, pois estava ficando conseguindo ajustar a tela

--- visual/src/App.jsx
+++ visual/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css';
 import './colors.css';
-import NavOff from './components/navbar/navbaroff.jsx';
-import MainOff from './components/main/mainoff.jsx';
 import NavIn from './components/navbar/navbarin.jsx';
-import Search from './components/search/search.jsx';
-import Favorite from './components/favorite/favorite.jsx';
 
 function App() {
   return (
     <div className="App main-color">
-      <NavIn/>
-      <Favorite/>
+      <NavIn className="nav"/>
     </div>
   );
 }
