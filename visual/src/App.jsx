0f132d05da5e975381ae26c4c0631c49a20f71ee
oliveira533 <go609127@gmail.com>
2023-04-27 20:15:37 -0300

    Deletando arquivos e começando denovo

--- visual/src/App.jsx
+++ visual/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import './colors.css';
+import MainIn from './components/main/main';
 import NavIn from './components/navbar/navbarin.jsx';
 
 function App() {
   return (
     <div className="App main-color">
       <NavIn className="nav"/>
+      <MainIn/>
     </div>
   );
 }
---
+++ visual/src/components/main/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './main.css'
+import '../../colors.css'
+
+function MainIn(){
+    return(
+        <div className="content font-color">
+            <h2>Principais escolhas</h2>
+            <div className="wrap">
+
+            </div>
+        </div>
+    )
+}
+
+export default MainIn;
