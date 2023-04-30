f34d44a1194f86ac9ed1c6d3679bd36570ee203a
oliveira533 <go609127@gmail.com>
2023-04-30 11:00:21 -0300

    Criando a nova tela principal

---
+++ visual/src/components/main/item/item.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './item.css';
+import '../../../colors.css'
+
+function Item(){
+    return(
+        <div className="item sec-color">
+            <img src="#" alt="" className="img" />
+            Lorem Ipsum
+        </div>
+    );
+};
+
+export default Item;
--- visual/src/components/main/main.jsx
+++ visual/src/components/main/main.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import './main.css'
 import '../../colors.css'
+import Item from "./item/item.jsx";
 
 function MainIn(){
     return(
         <div className="content font-color">
             <h2>Principais escolhas</h2>
             <div className="wrap">
-
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+            </div>
+            <h2>Novidades</h2>
+            <div className="wrap">
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
             </div>
         </div>
     )
