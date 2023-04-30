7fb2385443ee5def303d198cfed924c78ff9e632
oliveira533 <go609127@gmail.com>
2023-04-30 13:02:22 -0300

    Finalizado tela de pesquisa

--- visual/src/components/search/search.jsx
+++ visual/src/components/search/search.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./search.css";
 import "../../colors.css";
 import { BiSearchAlt2  } from "react-icons/bi";
+import {Item} from './item/item.jsx';
 
 export function Search(){
     return(
-        <div className="content main-color">
+        <div className="content main-color font-color">
             <div className="search sec-color">
-                <input type="text" className="background txb"/>
+                <input type="text" className="background txb font-color" placeholder="Pesquisa"/>
                 <button className="background"><BiSearchAlt2 className="font-color img"/></button>
+            </div>
+            <div className="wrap">
+                <Item/>
+                <Item/>
+                <Item/>
             </div>
         </div>
     )
