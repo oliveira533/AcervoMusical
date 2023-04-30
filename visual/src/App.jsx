534db6f40442850885f114ff139e68b5df64d4ec
oliveira533 <go609127@gmail.com>
2023-04-30 17:29:06 -0300

    Tela de de perfil finalizada

--- visual/src/App.jsx
+++ visual/src/App.jsx
@@ -3,13 +3,14 @@ import './colors.css';
 import { MainIn } from './components/main/main';
 import { NavIn , NavOff } from './components/navbar/navbar';
 import { Search } from './components/search/search';
-import { Fav } from './components/favorite/favorite'
+import { Fav } from './components/favorite/favorite';
+import { Profile } from './components/profile/profile';
 
 function App() {
   return (
     <div className="App main-color">
       <NavIn className="nav"/>
-      <Fav/>
+      <Profile/>
     </div>
   );
 }
--- visual/src/components/profile/profile.jsx
+++ visual/src/components/profile/profile.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './profile.css';
+import '../../colors.css';
+import { FaUser  } from "react-icons/fa";
 
-function Profile(){
+export function Profile(){
     return(
-        <div>
-            
+        <div className="content font-color">
+            <FaUser className="icon-user-edit"/>
+            <h1>User Name</h1>
+            <div className="desc">
+                <h2>Descrição</h2>
+                <textarea className="txa"></textarea>
+            </div>
         </div>
     );
 };
