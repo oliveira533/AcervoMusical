2d381e46c3e74e52861736b743bab482000bf7ff
oliveira533 <go609127@gmail.com>
2023-04-30 15:43:25 -0300

    Tela de favoritos criada

--- visual/src/App.jsx
+++ visual/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import './colors.css';
-import {MainIn} from './components/main/main';
-import {NavIn , NavOff} from './components/navbar/navbar.jsx';
+import { MainIn } from './components/main/main';
+import { NavIn , NavOff } from './components/navbar/navbar';
 import { Search } from './components/search/search';
+import { Fav } from './components/favorite/favorite'
 
 function App() {
   return (
     <div className="App main-color">
       <NavIn className="nav"/>
-      <Search/>
+      <Fav/>
     </div>
   );
 }
