4772ff0d12f2452ceb0b3f368f9b19e5e6c9a848
oliveira533 <go609127@gmail.com>
2023-05-14 13:40:12 -0300

    Tela de artista começada

--- visual/src/App.jsx
+++ visual/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <div className="App main-color">
       <NavIn className="nav"/>
       <MainIn/>
-      <Log/>
+      <LogOut/>
     </div>
   );
 }
---
+++ visual/src/components/artist/artist.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './artist.css';
+import '../../colors.css';
+
+export function Art(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
