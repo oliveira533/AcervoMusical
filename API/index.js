5436bcfa3b629c7218bb4ecdf3b419c80c239310
oliveira533 <go609127@gmail.com>
2023-06-13 18:43:25 -0300

    Caminho para criar usuário

--- API/index.js
+++ API/index.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'root',
+  password : '',
+  database : 'acervo'
 });
 
 // criando o app node
@@ -24,7 +24,30 @@ app.use(bodyParser.json());
 app.get('/api/exemplo', (req, res) =>{
     const exemplo = { mensagem: 'exemplo de api REST com node' };
     res.json(exemplo);
+});
+
+// primeira conexão com o banco de dados
+app.get('/api/connect', (req, res) =>{
+    connection.connect();
+    connection.query('SELECT * FROM ARTIST', function(error, results, fields){
+      // if (error) throw error;
+      // res.send(results);
+      console.log(results);
+      res.json(results);
+    });
+    connection.end();
+});
+
+// Caminho para criar usuário para cadastro
+app.post('/api/singin', (req, res)=>{
+    console.log(req.query)
+
+    connection.connect();
+    connection.query("INSERT INTO users(USRNAME, USREMAIL, USRPASSWORD) VALUES('"+req.query.name+"', '"+req.query.email+"', '"+req.query.password+"')");
+    connection.end();
+    res.send('Requisição enviada');
 })
+
 
 // iniciando o servidor 
 app.listen(PORT, ()=>{
