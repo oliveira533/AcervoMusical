cdbefc9d034b1d44e0e1c549ff8003c34ce8395e
oliveira533 <go609127@gmail.com>
2023-06-22 17:45:15 -0300

    Criando a rota de pesquisa no banco de dados

--- API/index.js
+++ API/index.js
@@ -84,7 +84,37 @@ app.get('/api/login', (req, res)=>{
     })
 });
 
-// Caminho para pesquisar arquivos
+app.get('/api/pesquisa', (req, res) => {
+  console.log('Chegou')
+  // Criando variável de consulta
+  sQuery = "SELECT MSCNAME FROM music WHERE MSCNAME LIKE '%" + req.query.value + "%' UNION SELECT ALBNAME FROM album WHERE ALBNAME LIKE '%" + req.query.value + "%' UNION SELECT BANNAME FROM band WHERE BANNAME LIKE '%" + req.query.value + "%' UNION SELECT (ARTALTEREGO) FROM artist LEFT JOIN internal ON artist.ARTID = internal.INTARTIST WHERE internal.INTARTIST IS NULL AND (ARTALTEREGO LIKE '%" + req.query.value + "%' OR ARTNAME LIKE '%" + req.query.value + "%') UNION SELECT (ARTALTEREGO) FROM artist LEFT JOIN internal ON ARTID = INTARTIST WHERE INTARTIST = ARTID AND (ARTALTEREGO LIKE '%" + req.query.value + "%' OR ARTNAME LIKE '%" + req.query.value + "%');";
+
+  // Criando uma nova conexão com o servidor para poder se conectar 
+  const con = mysql.createConnection({
+    // Configurações da conexão com o banco de dados
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'acervo'
+  });
+
+  // Conexão com o servidor de banco de dados
+  con.connect();
+
+  // Fazendo a requisição no banco
+  con.query(sQuery, function(error, results, fields) {
+    if (error) {
+      // Tratativa de erro da requisição
+      console.error(error);
+      res.status(500).send('Erro no servidor');
+      return;
+    }
+
+    res.json(results);
+
+    con.end();
+  });
+});
 
 
 // iniciando o servidor 
