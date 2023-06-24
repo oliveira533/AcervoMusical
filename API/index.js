d3feabbab07d82323f75af2faa6bfa131e33dce6
oliveira533 <go609127@gmail.com>
2023-06-24 12:20:22 -0300

    Fazendo restruturação geral no código

--- API/index.js
+++ API/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // conexão com o banco de dados
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -20,41 +20,48 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 
-// caminho de exemplo
-app.get('/api/exemplo', (req, res) =>{
-    const exemplo = { mensagem: 'exemplo de api REST com node' };
-    res.json(exemplo);
-});
 
-// primeira conexão com o banco de dados
-app.get('/api/connect', (req, res) =>{
-    connection.connect();
-    connection.query('SELECT * FROM ARTIST', function(error, results, fields){
-      // if (error) throw error;
-      // res.send(results);
-      console.log(results);
-      res.json(results);
-    });
-    connection.end();
-});
-
-// Caminho para criar usuário
+// Caminho para criar usuário USR-ADD
 app.post('/api/singin', (req, res)=>{
-    console.log(req.query)
-
+    sQuery= "INSERT INTO users(USRNAME, USREMAIL, USRPASSWORD) VALUES('"+req.query.name+"', '"+req.query.email+"', '"+req.query.password+"')";
+    
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database : 'acervo'
+    });
     connection.connect();
-    connection.query("INSERT INTO users(USRNAME, USREMAIL, USRPASSWORD) VALUES('"+req.query.name+"', '"+req.query.email+"', '"+req.query.password+"')");
-    connection.end();
-    res.send('Requisição enviada');
+    connection.query(sQuery, function(error, results, fields){
+      if(error){
+        console.error(error);
+        res.status(500).send('Erro ao inserir dado no banco. Erro: '+error);
+        return;
+      }
+      res.send('Requisição enviada');
+
+      connection.end();
+    });
 })
 
+// Caminho para efetuar login USR-LOG
 app.get('/api/login', (req, res)=>{
+    sQuery = "SELECT USRID, USREMAIL, USRPASSWORD FROM users WHERE USREMAIL LIKE '"+req.query.email+"'";
 
-    // Fazendo a consulta no banco de dados
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database : 'acervo'
+    });
     connection.connect();
-    connection.query(("SELECT USRID, USREMAIL, USRPASSWORD FROM users WHERE USREMAIL LIKE '"+req.query.email+"'"),function(error, results,fields){
-    connection.end();
-      if (error) throw error
+
+    connection.query(sQuery,function(error, results,fields){
+      if(error){
+        console.error(error);
+        res.status(500).send('Erro ao inserir dado no banco. Erro: '+error);
+        return;
+      }
 
       jsonData = JSON.stringify(results);
       // Acessando o campo USREMAIL
@@ -81,28 +88,24 @@ app.get('/api/login', (req, res)=>{
           'id': false,
           'logged': false
         })};
+      connection.end()
     })
 });
 
+// Caminho para efetuar login DATA-SER
 app.get('/api/pesquisa', (req, res) => {
-  // console.log('Chegou')
   // Criando variável de consulta
   sQuery = "SELECT MSCID ID,MSCNAME NOME FROM music WHERE MSCNAME LIKE '%" + req.query.value + "%' UNION SELECT ALBID ID,ALBNAME NOME FROM album WHERE ALBNAME LIKE '%" + req.query.value + "%' UNION SELECT BANID ID, BANNAME NOME FROM band WHERE BANNAME LIKE '%" + req.query.value + "%' UNION SELECT ARTID ID, ARTALTEREGO NOME FROM artist LEFT JOIN internal ON artist.ARTID = internal.INTARTIST WHERE internal.INTARTIST IS NULL AND (ARTALTEREGO LIKE '%" + req.query.value + "%' OR ARTNAME LIKE '%" + req.query.value + "%') UNION SELECT ARTID ID, ARTALTEREGO NOME FROM artist LEFT JOIN internal ON ARTID = INTARTIST WHERE INTARTIST = ARTID AND (ARTALTEREGO LIKE '%" + req.query.value + "%' OR ARTNAME LIKE '%" + req.query.value + "%');";
-
-  // Criando uma nova conexão com o servidor para poder se conectar 
-  const con = mysql.createConnection({
-    // Configurações da conexão com o banco de dados
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'acervo'
+  
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'acervo'
   });
+  connection.connect();
 
-  // Conexão com o servidor de banco de dados
-  con.connect();
-
-  // Fazendo a requisição no banco
-  con.query(sQuery, function(error, results, fields) {
+  connection.query(sQuery, function(error, results, fields) {
     if (error) {
       // Tratativa de erro da requisição
       console.error(error);
@@ -119,34 +122,32 @@ app.get('/api/pesquisa', (req, res) => {
 // Caminho para adicionar artista favorito
 app.post('/api/favorite/add/artist', (req, res) =>{
   sQuery= "INSERT INTO favorite(FAVUSER, FAVARTIST) VALUES ("+req.query.user+","+req.query.artist+")";
-  connection.connect();
+
+  fnCheckConnection();
   connection.query(sQuery, function(error, results, fields){
     if (error){
       console.error(error)
-      res.status(400).send('Erro ao inserir dado no banco. Erro: '+error);
+      res.status(500).send('Erro ao inserir dado no banco. Erro: '+error);
       return;
     }
 
     res.status(201).send(true);
-
-    connection.end()
   });
 });
 
 // Caminho para adicionar banda favorita
-app.post('api/favorite/add/band', (req, res) =>{
+app.post('/api/favorite/add/band', (req, res) =>{
   sQuery = "INSERT INTO favorite(FAVUSER, FAVBAND) VALUES ("+req.query.user+","+req.query.band+");";
-  connection.connect();
+
+  fnCheckConnection();
   connection.query(sQuery, function(error, results, fields){
     if (error){
       console.error(error);
-      res.status(400).send('Erro ao inserir dado no banco. Erro: '+error);
+      res.status(500).send('Erro ao inserir dado no banco. Erro: '+error);
       return;
     }
     
     res.status(201).send(true);
-
-    connection.end();
   });
 });
 
